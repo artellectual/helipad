af68035b30c2061db794c05220080dbe728c4000
Zack Siri <zacksiri@gmail.com>
2021-01-25 21:14:32 +0700

    Added test env var

--- config/releases.exs
+++ config/releases.exs
@@ -4,6 +4,9 @@ config :helipad, Helipad.Repo,
   # ssl: true,
   url: System.fetch_env!("DATABASE_URL")
 
+config :helipad, :test
+  value: System.fetch_env!("TEST")
+
 config :helipad, HelipadWeb.Endpoint,
   http: [:inet6, port: 4000],
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
