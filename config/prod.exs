0425a74d8a581d2de3eb403d038ef9aa30402ca3
Zack Siri <zacksiri@gmail.com>
2021-01-28 21:10:38 +0700

    config prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :helipad, HelipadWeb.Endpoint,
-  url: [host: "example.com", port: 4000],
-  server: true,
+  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
