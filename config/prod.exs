e2bf42bb229a7122404031eab20e9f9b716c0696
Zack Siri <zacksiri@gmail.com>
2021-01-28 21:25:30 +0700

    fix config

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :helipad, HelipadWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  server: true
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
